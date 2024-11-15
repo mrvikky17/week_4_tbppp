550684cc79ec00c9548a2a3d35abc9a8c3e4ca63
mrvikky17 <vikkykushwah172003@gmail.com>
2024-11-15 22:38:33 +0530

    week4

---
+++ data/data.js
@@ -0,0 +1,17 @@
+const studendetails=[
+    {
+        name:"abcde",
+        email:"xyz@gmail.com",
+        password:909090,
+        rollno:90
+
+},
+{
+        name:"abe",
+        email:"hhh@gmail.com",
+        password:9098880,
+        rollno:88
+
+}
+]
+module.exports=studendetails
